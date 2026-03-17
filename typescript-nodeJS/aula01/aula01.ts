75392bc73193845b43766bb91571fcb0041e6930
luizincodes <ohuluizin@outlook.com>
2026-03-17 18:46:38 -0300

    aprendi tipos de dados, variáveis, operadores de comparação e operadores lógicos...

---
+++ typescript-nodeJS/aula01/aula01.ts
@@ -0,0 +1,44 @@
+// variáveis:
+
+/*
+let: usamos o let quando vamos alterar o valor da variável ao longo do código
+const: usamos o const quando NÃO vamos alterar o valor da variável ao longo do código.
+
+ao tentar-mos alterar o valor da variável const, certamente retornará um erro.
+
+*/
+
+let idade: number = 22;
+const nome: string = "luiz";
+
+/* 
+
+Podemos criar variáveis sem definir um valor (não funciona com o const, pois depois a variável precisaria receber um valor, e o const não aceita.)
+
+*/
+
+let senha: string;
+let email: string;
+
+console.log("Olá Mundo")
+
+/* 
+
+Operadores de comparação em typescript:
+
+== : comparação de valor
+=== : comparação de valor e tipo
+!= :  diferença de
+> : maior que
+< : menor que
+>= : maior ou igual a
+<= : menor ou igual a
+
+operadores lógicos:
+
+&& : operador lógico AND (E)
+|| : operador lógico OR (OU)
+! : operador lógico NOT (NÃO)
+
+
+*/
